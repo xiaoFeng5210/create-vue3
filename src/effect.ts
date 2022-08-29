3bc0b94b69660aea33ef4e800aba48d9dc24d2f4
Eadweard <804875728@qq.com>
2022-08-29 22:44:27 +0800

    实现effect trigger track

--- src/effect.ts
+++ src/effect.ts
@@ -5,10 +5,34 @@ class ReactiveEffect {
     this._fn = fn;
   }  
   run() {
+    activeEffect = this;
     this._fn()
   }
 }
+const targetMap = new Map()
+export function track(target, key) {
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+}
+let activeEffect;
 
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target)
+  let dep = depsMap.get(key)
+  for (const effect of dep) {
+    effect.run()
+  }
+
+}
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
   _effect.run()
